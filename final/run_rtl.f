9a0bdefd6b6b2b715f908a1447be83484280919f
strong-ting <s8527502987@gmail.com>
2021-06-26 22:57:43 +0800

    -del sim dir

--- final/run_rtl.f
+++
@@ -1,5 +0,0 @@
-./testfixture_60fps.v  
-./LEDDC.v 
-./sram_512x16/sram_512x16.v  
-./sram_256x16/sram_256x16.v  
--exit
--- final/run_syn_30.f
+++
@@ -1,6 +0,0 @@
-./testfixture_30fps.v
-./LEDDC_syn.v
-./sram_512x16/sram_512x16.v
-./sram_256x16/sram_256x16.v 
-/usr/cad/CBDK_IC_Contest_v2.1/Verilog/tsmc13_neg.v 
-+define+SDF 
